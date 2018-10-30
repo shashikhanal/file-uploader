cc4a5496f01aec930f589b4abb0c4d46c12e62ac
shashikhanal <khanalshashi@gmail.com>
2018-10-30 17:53:23 +0545

    uploadFileController refactored and uploadFileService created

--- src/controllers/uploadFileController.js
+++ src/controllers/uploadFileController.js
@@ -1,47 +1,30 @@
 import { Router } from 'express';
 import HttpStatus from 'http-status-codes';
+import * as uploadFileService from '../services/uploadFileService';
 import { fileHeaderValidator } from '../validators/fileHeaderValidator';
 import { fileTypeValidator } from '../validators/fileTypeValidator';
 let multer = require('multer');
 let upload = multer({ dest: 'public/images/' });
-let fs = require('fs');
 
 const router = Router();
-const IMAGE_FORMAT = `.webp`;
 
 /**
  * Uploads image
  */
 router.post('/', fileHeaderValidator, upload.single('name'), fileTypeValidator, (req, res, next) => {
-  let tmpPath = req.file.path;
-  let targetPath = 'public/images/' + req.get('x-test') + IMAGE_FORMAT;
+  try {
+    let response = uploadFileService.uploadFile(req);
 
-  let src = fs.createReadStream(tmpPath);
-  let dest = fs.createWriteStream(targetPath);
-  src.pipe(dest);
-
-  let hadError = false;
-  src.on('error', function() {
-    hadError = true;
-
-    res.status(HttpStatus.ERROR).json({
-      status: HttpStatus.ERROR,
-      success: false,
-      message: req.file.originalname + ' is not uploaded.'
-    });
-  });
-
-  src.on('end', function() {
-    if (!hadError) {
-      fs.unlink(tmpPath);
-
+    if (response) {
       res.status(HttpStatus.OK).json({
         status: HttpStatus.OK,
         success: true,
         message: req.file.originalname + ' is uploaded successfully.'
       });
     }
-  });
+  } catch (err) {
+    next(err);
+  }
 });
 
 export default router;
---
+++ src/services/uploadFileService.js
@@ -0,0 +1,28 @@
+import Boom from 'boom';
+let fs = require('fs');
+
+const IMAGE_FORMAT = `.webp`;
+
+export async function uploadFile(req) {
+  let tmpPath = req.file.path;
+  let targetPath = 'public/images/' + req.get('x-test') + IMAGE_FORMAT;
+
+  let src = fs.createReadStream(tmpPath);
+  let dest = fs.createWriteStream(targetPath);
+  await src.pipe(dest);
+
+  let hadError = false;
+  src.on('error', function() {
+    hadError = true;
+
+    throw new Boom('Error uploading image.');
+  });
+
+  return src.on('end', function() {
+    if (!hadError) {
+      fs.unlink(tmpPath);
+
+      return true;
+    }
+  });
+}
